5a167e3beb175744509703becfc597067df83b62
Dandi Lesmana <dandilesmana.if21@gmail.com>
2023-01-14 05:46:12 -0800

    update

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # PHP-basic
-Panduan belajar dasar pemrograman PHP 
+Repositori untuk belajar pemrograman PHP 
+
+1.Skrip PHP dimulai dengan <?php dan diakhiri dengan ?>
---
+++ hello.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+
+<?php
+echo "Hello php" . "<br>";
+echo "hello1";
+ECHO "Hello2<br>";
+echo "Hello3<br>";
+
+?>
+
+</body>
+</html>
+
